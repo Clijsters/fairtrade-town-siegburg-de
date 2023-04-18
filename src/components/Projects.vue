ad242d80948c2708842a89088a91ea1ef16444e1
Dominique Clijsters <dominique.clijsters@bdr.de>
2023-04-18 15:25:23 +0200

    Zwischenstand m Programm

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -6,7 +6,7 @@
         <div class="column--2 projects__offset" />
         <div class="projects__info column-mobile--12 column--8">
           <h3>Wann und Wo?</h3>
-          <p>Die Auszeichnungsfeier findet statt am <strong>12.05.2023 ab ??:00h</strong> in den R'umlichkeiten des KSI
+          <p>Die Auszeichnungsfeier findet statt am <strong>12.05.2023 ab 18:00h</strong> in den Räumlichkeiten des KSI
             auf dem Michaelsberg</p>
         </div>
       </div>
@@ -14,73 +14,175 @@
         <div class="column--2 projects__offset" />
         <div class="projects__info column-mobile--12 column--8">
           <h3>Programm</h3>
-          <p>Das Programm besteht aus der Auszeichnung, einer Rede, Live-Musik und kurzen Beiträgen der Teilnehmer:innen:</p>
+          <p>Das Programm besteht aus der Auszeichnung, einer Rede, Live-Musik und kurzen Beiträgen der Teilnehmer:innen:
+          </p>
           <img src="../assets/programm-pre.png">
           <p></p>
-          <h4>Sektempfang</h4>
-          <p>
-            Der Einlass beginnt mit einem kleinen Sektempfang. Bei gutem Wetter auf der großen Terasse mit Aussicht über
-            Siegburg. Alternativ in den Räumlichkeiten der Veranstaltung.
-          </p>
-          <h4>Grußwort des Bürgermeisters</h4>
-          <p>
-            Lorem Ipsum
-          </p>
-          <h4>Vortrag FairTrade e.V.</h4>
-          <p>
-            Lorem Ipsum
-          </p>
-          <h4>Cello - Tereza Mikusova</h4>
-          <p>
-            Das Programm wird begleitet von einer Cellistin, welche moderne und klassische Musik spielt.
-          </p>
-          <h4>„Siegburg packt’s ohne ein“ – Warum nachhaltig wirtschaften und fairer Handel in Siegburg Perspektive haben
-          </h4>
-          <p>
-            Dominique Clijsters, Geschäftsführer des Unverpacktladens in der Kaiserstraße erzählt in einem Kurzvortrag,
-            warum er glaubt, dass vor allem Mittelstädte wie Seigburg von nachhaltigem Wirtschaften und fairem Handel
-            profitieren.
-          </p>
-          <h4>Vortrag Indienhilfe Anno Gymnasium Siegburg</h4>
-          <p>
-            Lorem Ipsum
-          </p>
-          <h4>Vortrag zu fairem Kaffee - Cofi Loco</h4>
-          <p>
-            Lorem Ipsum
-          </p>
-          <h4>Cello 2</h4>
-          <p>
-            Lorem Ipsum
-          </p>
-          <h4>Interview / Kurzvortrag zum Fair-O-Mat des Anno-Gymnasium Siegburg</h4>
-          <p>
-            Lorem Ipsum
-          </p>
-          <h4>Interview / Kurzvortrag Projektkurs Afrika Gymnasium Alleestraße</h4>
-          <p>
-            Lorem Ipsum
-          </p>
-          <h4>Karibu - Sounds of Marimbas</h4>
-          <p>
-            Lorem Ipsum
-          </p>
-          <h4>Staende mit Proben und Snacks</h4>
-          <p>
-            Lorem Ipsum
-          </p>
-          <h4>20:30 - feierliche Auszeichnung</h4>
-          <p>
-            Lorem Ipsum
-          </p>
-          <h4>Ausklang Schulcombo Gymnasium Alleestraße</h4>
-          <p>
-            Lorem Ipsum
-          </p>
-          <h4>Open End im Anno17</h4>
-          <p>
-            Lorem Ipsum
-          </p>
+
+          <div class="row row--multiline">
+            <div class="column--1 column-mobile--2 column-tablet--2">18:00h</div>
+            <div class="column--3 column-mobile--12 column-tablet--12">
+              <h4>Sektempfang</h4>
+            </div>
+            <div class="column--8 column-mobile--12 column-tablet--12">
+              <p>
+                Der Einlass beginnt mit einem kleinen Sektempfang. Bei gutem Wetter auf der großen Terasse mit Aussicht
+                über Siegburg. Alternativ in den Räumlichkeiten der Veranstaltung.
+              </p>
+            </div>
+          </div>
+
+
+          <div class="row row--multiline">
+            <div class="column--1 column-mobile--2 column-tablet--2">19:00h</div>
+            <div class="column--3 column-mobile--12 column-tablet--12">
+              <h4>Grußwort des Bürgermeisters</h4>
+            </div>
+            <div class="column--8 column-mobile--12 column-tablet--12">
+              <p>
+                Lorem Ipsum
+              </p>
+            </div>
+          </div>
+          <div class="row row--multiline">
+            <div class="column--1 column-mobile--2 column-tablet--2">19:10h</div>
+            <div class="column--3 column-mobile--12 column-tablet--12">
+              <h4>Vortrag FairTrade e.V.</h4>
+            </div>
+            <div class="column--8 column-mobile--12 column-tablet--12">
+              <p>
+                Lorem Ipsum
+              </p>
+            </div>
+          </div>
+          <div class="row row--multiline">
+            <div class="column--1 column-mobile--2 column-tablet--2">19:20h</div>
+            <div class="column--3 column-mobile--12 column-tablet--12">
+              <h4>Cello - Tereza Mikusova</h4>
+            </div>
+            <div class="column--8 column-mobile--12 column-tablet--12">
+              <p>
+                Das Programm wird begleitet von einer Cellistin, welche moderne und klassische Musik spielt.
+              </p>
+            </div>
+          </div>
+          <div class="row row--multiline">
+            <div class="column--1 column-mobile--2 column-tablet--2">19:30h</div>
+            <div class="column--3 column-mobile--12 column-tablet--12">
+              <h4>„Siegburg packt’s ohne ein“ – Warum nachhaltig wirtschaften und fairer Handel in Siegburg Perspektive
+                haben
+              </h4>
+            </div>
+            <div class="column--8 column-mobile--12 column-tablet--12">
+              <p>
+                Dominique Clijsters, Geschäftsführer des Unverpacktladens in der Kaiserstraße erzählt in einem
+                Kurzvortrag,
+                warum er glaubt, dass vor allem Mittelstädte wie Seigburg von nachhaltigem Wirtschaften und fairem Handel
+                profitieren.
+              </p>
+            </div>
+          </div>
+          <div class="row row--multiline">
+            <div class="column--1 column-mobile--2 column-tablet--2">19:40h</div>
+            <div class="column--3 column-mobile--12 column-tablet--12">
+              <h4>Markt der möglichkeiten</h4>
+            </div>
+            <div class="column--8 column-mobile--12 column-tablet--12">
+              <p>
+                Lorem Ipsum
+              </p>
+            </div>
+          </div>
+          <div class="row row--multiline">
+            <div class="column--1 column-mobile--2 column-tablet--2">20:00h</div>
+            <div class="column--3 column-mobile--12 column-tablet--12">
+              <h4>Vortrag Indienhilfe Anno Gymnasium Siegburg</h4>
+            </div>
+            <div class="column--8 column-mobile--12 column-tablet--12">
+              <p>
+                Lorem Ipsum
+              </p>
+            </div>
+          </div>
+          <div class="row row--multiline">
+            <div class="column--1 column-mobile--2 column-tablet--2">20:05h</div>
+            <div class="column--3 column-mobile--12 column-tablet--12">
+              <h4>Vortrag zu fairem Kaffee - Cofi Loco</h4>
+            </div>
+            <div class="column--8 column-mobile--12 column-tablet--12">
+              <p>
+                Lorem Ipsum
+              </p>
+            </div>
+          </div>
+          <div class="row row--multiline">
+            <div class="column--1 column-mobile--2 column-tablet--2">20:15h</div>
+            <div class="column--3 column-mobile--12 column-tablet--12">
+              <h4>Cello 2</h4>
+            </div>
+            <div class="column--8 column-mobile--12 column-tablet--12">
+              <p>
+                Lorem Ipsum
+              </p>
+            </div>
+          </div>
+          <div class="row row--multiline">
+            <div class="column--1 column-mobile--2 column-tablet--2">20:20h</div>
+            <div class="column--3 column-mobile--12 column-tablet--12">
+              <h4>Interview / Kurzvortrag zum Fair-O-Mat des Anno-Gymnasium Siegburg</h4>
+            </div>
+            <div class="column--8 column-mobile--12 column-tablet--12">
+              <p>
+                Lorem Ipsum
+              </p>
+            </div>
+          </div>
+          <div class="row row--multiline">
+            <div class="column--1 column-mobile--2 column-tablet--2">20:25h</div>
+            <div class="column--3 column-mobile--12 column-tablet--12">
+              <h4>Interview / Kurzvortrag Projektkurs Afrika Gymnasium Alleestraße</h4>
+            </div>
+            <div class="column--8 column-mobile--12 column-tablet--12">
+              <p>
+                Lorem Ipsum
+              </p>
+            </div>
+          </div>
+          <div class="row row--multiline">
+            <div class="column--1 column-mobile--2 column-tablet--2">20:30h</div>
+            <div class="column--3 column-mobile--12 column-tablet--12">
+              <h4>Offizielle Auszeichnung</h4>
+            </div>
+            <div class="column--8 column-mobile--12 column-tablet--12">
+              <p>
+                Lorem Ipsum
+              </p>
+            </div>
+          </div>
+
+          <div class="row row--multiline">
+            <div class="column--1 column-mobile--2 column-tablet--2">20:35h</div>
+            <div class="column--3 column-mobile--12 column-tablet--12">
+              <h4>Ausklang Schulcombo Gymnasium Alleestraße</h4>
+            </div>
+            <div class="column--8 column-mobile--12 column-tablet--12">
+              <p>
+                Lorem Ipsum
+              </p>
+            </div>
+          </div>
+          <div class="row row--multiline">
+            <div class="column--1 column-mobile--2 column-tablet--2">???</div>
+            <div class="column--3 column-mobile--12 column-tablet--12">
+              <h4>Open End im Anno17</h4>
+            </div>
+            <div class="column--8 column-mobile--12 column-tablet--12">
+              <p>
+                Lorem Ipsum
+              </p>
+            </div>
+          </div>
+
         </div>
       </div>
       <div class="row">
@@ -108,21 +210,21 @@
         <div class="row"><br></div>
       </div>
       <div class="row row--multiline">
-        <div class="column-desktop--1"></div>
-        <div class="column--2 column-mobile--4">
+        <div class="column--1 projects__offset" />
+        <div class="column--2 column-mobile--4 column-tablet--4">
           <a href="https://siegburg.de/wirtschaft-handel/wirtschaftsfoerderung/fairtradetown/index.html" target="_blank"
             rel="nofollow">
             <img class="contact__image" src="../assets/ftts-logo-blau.png" alt="mail-Logo">
             <p class="contact__image__description">Stadt Siegburg</p>
           </a>
         </div>
-        <div class="column--2 column-mobile--4">
+        <div class="column--2 column-mobile--4 column-tablet--4">
           <a href="https://www.naturata-bioladen.de/biolaeden/naturata-siegburg/" target="_blank" rel="nofollow">
             <img class="contact__image" src="../assets/naturata_logo.png" alt="mail-Logo">
             <p class="contact__image__description">Naturata Siegburg</p>
           </a>
         </div>
-        <div class="column--2 column-mobile--4">
+        <div class="column--2 column-mobile--4 column-tablet--4">
           <a href="https://siegburg-unverpackt.de/" target="_blank" rel="nofollow">
             <img class="contact__image" src="../assets/Siegburg-Unverpackt-Logo.png" alt="mail-Logo">
             <p class="contact__image__description">Siegburg Unverpackt</p>
@@ -134,13 +236,12 @@
             <p class="contact__image__description">Cofi Loco</p>
           </a>
         </div>
-        <div class="column--2 column-mobile--4">
+        <div class="column--2 column-mobile--4 column-tablet--4">
           <a href="https://www.fairtrade-towns.de/aktuelles" target="_blank" rel="nofollow">
             <img class="contact__image" src="../assets/ftde-logo.png" alt="mail-Logo">
             <p class="contact__image__description">Fairtrade Deutschland e.V.</p>
           </a>
         </div>
-        <div class="column--1"></div>
       </div>
     </div>
   </section>
